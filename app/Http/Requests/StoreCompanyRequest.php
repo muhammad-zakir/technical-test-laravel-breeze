7440aa48efb281684a3676cabe2ef7905be998c2
Muhammad Zakir <hi@zakir.id>
2026-04-01 16:21:37 +0700

    Add requests

---
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompanyRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['nullable', 'email', 'max:255'],
+            'logo' => ['nullable', 'image', 'max:2048'],
+            'website' => ['nullable', 'url', 'max:255'],
+        ];
+    }
+}
