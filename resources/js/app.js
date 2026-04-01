01f01ee880c573a34dea84039bbbe1a5c71f36d9
Muhammad Zakir <hi@zakir.id>
2026-04-01 16:03:22 +0700

    Integrate Ant Design

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +21,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Antd)
             .mount(el);
     },
     progress: {
